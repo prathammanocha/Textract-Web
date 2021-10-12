faedbd7330f4dfb8a8046e17e6991a47fb2d7ebd
Pratham Manocha <53337404+prathamystic@users.noreply.github.com>
2021-10-13 00:36:48 +0530

    Add files via upload

--- FrontEnd/Index.js
+++ FrontEnd/Index.js
@@ -1,1 +1,11 @@
+const fileForm = document.querySelector("#fileForm")
+const fileInput = document.querySelector("#fileInput")
 
+fileForm.addEventListener("submit", async Event => {
+    Event.preventDefault()
+    const file = fileInput.files[0]
+
+    const { url } = await fetch("./s3Url").then(res => res.json())
+    console.log({url})
+
+ })
